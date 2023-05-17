769f045b2bcb8c8c919427ccf7dc726da485462e
do_xuan_trung_iMac <trung13988@gmail.com>
2023-05-17 17:27:04 +0700

    MAIN -  add enable cors

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,10 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
     const app = await NestFactory.create(AppModule);
+    app.enableCors({
+        origin: process.env.CORS_ORIGINS.split(','),
+        optionsSuccessStatus: 200,
+    });
     await app.listen(process.env.PORT || 4000);
 }
 bootstrap();
