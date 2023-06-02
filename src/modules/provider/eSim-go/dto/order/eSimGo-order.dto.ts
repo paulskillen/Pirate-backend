e1915dd812fdf6db2832622d91db30c4a9e17848
do_xuan_trung_iMac <trung13988@gmail.com>
2023-06-02 13:28:37 +0700

    ESIM GO - add some apis

--- src/modules/provider/eSim-go/dto/order/eSimGo-order.dto.ts
+++ src/modules/provider/eSim-go/dto/order/eSimGo-order.dto.ts
@@ -5,3 +5,10 @@ export class ESimGoOrderInput {
 
     Order: { type: 'bundle'; item: string; quantity: number }[];
 }
+
+export class ESimGoApplyBundleToEsimInput {
+    iccid: string; // ICCID of target eSIM to apply Bundle to. If not provided, a new eSIM will be assigned to you.
+    name: string; // Name of Bundle to apply
+    startTime: string; // When the Bundle should start (if left empty, Bundle will start immediately),Format as defined in https://datatracker.ietf.org/doc/html/rfc3339#section-5.6, section 5.6. Timezone is UTC (ends with 'Z'). Example: 2006-01-02T15:04:05Z
+    repeat?: number; // How many bundles should be applied (each returns a new eSIM)
+}
