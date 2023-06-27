4c4a13778f9ad0519bdb92e98367e72b6d0d4ca5
do_xuan_trung_iMac <trung13988@gmail.com>
2023-06-27 11:39:51 +0700

    add email content

--- src/modules/email/email.constant.ts
+++ src/modules/email/email.constant.ts
@@ -7,19 +7,48 @@ export enum RefEmail {
 export const INTERNAL_EMAIL_SENDER =
     '"Pirate Mobile" <no-reply@piratemobile.gg>';
 
-export const EMAIL_ORDER_REFERENCES_TEMPLATE = () => `Hi :receiveFn,<br/>
-<br/>
-You've received a new event request (ID #:requestId) from :employeeNicknameId with the following detail:<br/>
+export const EMAIL_ORDER_REFERENCES_TEMPLATE = () => `
+<p>
+Thank you for choosing Pirate Mobile <br/>
+Please see attached the QR code to activate your eSim. <br/>
+Here are the steps a to activate an eSIM using a QR code:<br/>
+</p>
 
-<p><span>test</span><b>123</b></p>
-
-<img style="width:250px;height:250px" alt="Qr Code" src="data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAQAAAAEAAQMAAABmvDolAAAABlBMVEX///8AAABVwtN+AAAB8klEQVR42uyZMbLzIAyE10NByRE4CjeL7ZtxFI5AScF4/5Eg+Z1MqldFHqt5b8LXEEmrFcEdd9zxt1hJssVMlpXMJDcE+YzXAgAsDQmIm6+JDUsPeR7YARy5t5gRysKaHHkg6KUNAmTFQtI0ALDGna9kXQ/QmozSSntD6r4AX4r214GpDz2Uh6/Jtbj3rwLy08AIAXj4Kv8v/Yuk/zrQQ5arIhT40VmOXP6X3DWA1THrSY1sgfBlZQUbbAEdSWTwNZnwQODS4mYNOHzBSx+4AenceiaAqQ+OBXLSopTc9QCnqiEj6dDOKvLNvLkgAwAc8ypXHC6oI+5kPguIBWB1NY37kQ0ykrSltDTtAJj2QLQaSI5FR5K4hCsBKwIPqUnqSGLTbL4tQQYATEcqnCe7Lw9A82sJeNbky8CJymWeRpIRANPkLC1k8QV9uIRLATJyZfL2oCO3D/9wFnMLwMqaxionJ7lrNnmuSQsAyCy9NAfr0AeeZdACsE5LMx6i1GKP9K2XAuaup9uPr+heR9JxKloLwHSkFEsnACLHGwhMASd74KtuEX2MWVPA8z2K3PzYRPmxHVwHiNlVTON96EIRDQLP1Mkqt7mavrwu/jYwajLrH7UHGB7VFsCx/Uh6hlTsPeTj84ce68Add9zxHv8CAAD//6pM/a7UF8PwAAAAAElFTkSuQmCC" />
+<img style="width:250px;height:250px" alt="Qr Code" src="cid:@esimQrCode" />
 
 <p>
-You can approve or reject this request by the clicking the following link: :link<br/>
+1. Ensure that their device is eSIM compatible. They can check their device specifications or contact their carrier to confirm.
 </p>
-<br/>
-Please do not reply to this email as it's an automatic system email.<br/> 
-<br/>
-Sincerely,<br/> 
-System.`;
+
+<p>
+2. Obtain a QR code from their carrier. The QR code may come in the form of a physical card or as an email attachment.
+</p>
+
+<p>
+3. On their device, go to the “Settings” menu, then select “Mobile Data” or “Cellular Data”.
+</p>
+
+<p>
+4. Tap “Add Data Plan”.
+</p>
+
+<p>
+5. Scan the QR code provided by their carrier.
+</p>
+
+<p>
+6. Review the details of the eSIM plan and tap “Add Cellular Plan”.
+</p>
+
+<p>
+7. Follow the prompts to complete the activation process.
+</p>
+
+<p>
+8. Once the process is complete, the eSIM should be successfully activated on their device.
+</p>
+
+<p>
+It's important to note that the steps for activating an eSIM using a QR code may vary depending on the device or carrier. Customers should follow any additional instructions provided by their carrier or device manufacturer to ensure successful activation.
+</p>
+`;
--- src/modules/email/email.service.ts
+++ src/modules/email/email.service.ts
@@ -2,6 +2,7 @@ import { Injectable } from '@nestjs/common';
 import { EmailInputDto } from './dto/email.dto';
 import { EMAIL_DEFAULT } from './email.constant';
 import emailClient from './node-ses/node-ses';
+import NodeMailer from './node-mailer/node-mailer';
 
 const options: any = {
     from: 'no-reply@jobs.dermaster.co.th',
@@ -15,22 +16,50 @@ const options: any = {
     //   altText: '',
 };
 
+const qrCodeBase64 =
+    'iVBORw0KGgoAAAANSUhEUgAAAQAAAAEAAQMAAABmvDolAAAABlBMVEX///8AAABVwtN+AAAB8klEQVR42uyZMbLzIAyE10NByRE4CjeL7ZtxFI5AScF4/5Eg+Z1MqldFHqt5b8LXEEmrFcEdd9zxt1hJssVMlpXMJDcE+YzXAgAsDQmIm6+JDUsPeR7YARy5t5gRysKaHHkg6KUNAmTFQtI0ALDGna9kXQ/QmozSSntD6r4AX4r214GpDz2Uh6/Jtbj3rwLy08AIAXj4Kv8v/Yuk/zrQQ5arIhT40VmOXP6X3DWA1THrSY1sgfBlZQUbbAEdSWTwNZnwQODS4mYNOHzBSx+4AenceiaAqQ+OBXLSopTc9QCnqiEj6dDOKvLNvLkgAwAc8ypXHC6oI+5kPguIBWB1NY37kQ0ykrSltDTtAJj2QLQaSI5FR5K4hCsBKwIPqUnqSGLTbL4tQQYATEcqnCe7Lw9A82sJeNbky8CJymWeRpIRANPkLC1k8QV9uIRLATJyZfL2oCO3D/9wFnMLwMqaxionJ7lrNnmuSQsAyCy9NAfr0AeeZdACsE5LMx6i1GKP9K2XAuaup9uPr+heR9JxKloLwHSkFEsnACLHGwhMASd74KtuEX2MWVPA8z2K3PzYRPmxHVwHiNlVTON96EIRDQLP1Mkqt7mavrwu/jYwajLrH7UHGB7VFsCx/Uh6hlTsPeTj84ce68Add9zxHv8CAAD//6pM/a7UF8PwAAAAAElFTkSuQmCC';
+
 @Injectable()
 export class EmailService {
     async create(input: EmailInputDto): Promise<boolean> {
-        const { from, to, subject, message } = input;
-        const res = await emailClient.sendEmail(
-            {
+        // const { from, to, subject, message } = input;
+        // const res = await emailClient.sendEmail(
+        //     {
+        //         from: from || EMAIL_DEFAULT,
+        //         to,
+        //         subject,
+        //         message,
+        //     },
+        //     (res) => {
+        //         console.log({ res });
+        //     },
+        // );
+        // console.log({ res });
+        // return true;
+
+        try {
+            const { from, to, subject, message } = input;
+            const info = await NodeMailer.sendMail({
                 from: from || EMAIL_DEFAULT,
                 to,
                 subject,
-                message,
-            },
-            (res) => {
-                console.log({ res });
-            },
-        );
-        console.log({ res });
-        return true;
+                html: message,
+                attachments: [
+                    {
+                        // encoded string as an attachment
+                        filename: 'qrCode.png',
+                        path: `data:image/png;base64,${qrCodeBase64}`,
+                        content:
+                            'iVBORw0KGgoAAAANSUhEUgAAAQAAAAEAAQMAAABmvDolAAAABlBMVEX///8AAABVwtN+AAAB8klEQVR42uyZMbLzIAyE10NByRE4CjeL7ZtxFI5AScF4/5Eg+Z1MqldFHqt5b8LXEEmrFcEdd9zxt1hJssVMlpXMJDcE+YzXAgAsDQmIm6+JDUsPeR7YARy5t5gRysKaHHkg6KUNAmTFQtI0ALDGna9kXQ/QmozSSntD6r4AX4r214GpDz2Uh6/Jtbj3rwLy08AIAXj4Kv8v/Yuk/zrQQ5arIhT40VmOXP6X3DWA1THrSY1sgfBlZQUbbAEdSWTwNZnwQODS4mYNOHzBSx+4AenceiaAqQ+OBXLSopTc9QCnqiEj6dDOKvLNvLkgAwAc8ypXHC6oI+5kPguIBWB1NY37kQ0ykrSltDTtAJj2QLQaSI5FR5K4hCsBKwIPqUnqSGLTbL4tQQYATEcqnCe7Lw9A82sJeNbky8CJymWeRpIRANPkLC1k8QV9uIRLATJyZfL2oCO3D/9wFnMLwMqaxionJ7lrNnmuSQsAyCy9NAfr0AeeZdACsE5LMx6i1GKP9K2XAuaup9uPr+heR9JxKloLwHSkFEsnACLHGwhMASd74KtuEX2MWVPA8z2K3PzYRPmxHVwHiNlVTON96EIRDQLP1Mkqt7mavrwu/jYwajLrH7UHGB7VFsCx/Uh6hlTsPeTj84ce68Add9zxHv8CAAD//6pM/a7UF8PwAAAAAElFTkSuQmCC',
+                        encoding: 'base64',
+                        cid: '@esimQrCode',
+                    },
+                ],
+            });
+            console.log('Email sent:', info);
+            return true;
+        } catch (error) {
+            console.error('Error sending email:', error);
+        }
     }
 }
