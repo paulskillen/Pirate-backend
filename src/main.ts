4e6a91849f30a0adc43ba3e5b0f6cd727d74a386
do_xuan_trung_iMac <trung13988@gmail.com>
2023-12-08 12:42:04 +0700

    add console.log

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,12 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
     const app = await NestFactory.create(AppModule);
+    const cors = process.env.CORS_ORIGINS.split(',');
+
+    console.log(
+        '🚀 >>>>>> file: main.ts:8 >>>>>> bootstrap >>>>>> cors:',
+        cors,
+    );
     app.enableCors({
         origin: process.env.CORS_ORIGINS.split(','),
         optionsSuccessStatus: 200,
